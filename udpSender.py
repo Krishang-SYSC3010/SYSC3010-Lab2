7a3279e9adb0203f07b900dfb68493e3b16c7d35
Krishang <55495615+Krishang-SYSC3010@users.noreply.github.com>
2019-09-20 14:25:48 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -9,14 +9,18 @@ n = int(sys.argv[3])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
+s.bind((server_address)
 
 while 1:
-    print ("Enter data to transmit: ENTER to quit")
-    data = sys.stdin.readline().strip()
-    
     if not len(data):
-        break
-        
+       break
+       
+    data = sys.stdin.readline().strip()
+    data, address = sock.recvfrom(4096)
+       
+    if data:
+       sent = sock.sendto(data, address)
+   
     for i in range (1, n + 1):
         data1 = data + "(" + str(i) + ")"
         print ("data sent: " + data)
