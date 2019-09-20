430d6b0d8687240510ce850c4d01ba444c2a36a7
Krishang <55495615+Krishang-SYSC3010@users.noreply.github.com>
2019-09-20 18:00:38 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -1,29 +1,26 @@
- Source: https://pymotw.com/2/socket/udp.html
+Source: https://pymotw.com/2/socket/udp.html
 
 import socket, sys, time
 
 host = sys.argv[1]
 textport = sys.argv[2]
-n = int(sys.argv[3])
+n = sys.argv[3]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
-s.bind((server_address)
 
+test = ('localhost', 1007)
+s.bind((test)
+       
 while 1:
+    data = sys.stdin.readline().strip()
     if not len(data):
        break
-       
-    data = sys.stdin.readline().strip()
-    data, address = sock.recvfrom(4096)
-       
+    for i in range (1, n + 1)
+       data = "Message " + str(i)
+    #  s.sendall(data.encode('utf-8'))
+       s.sendto(data1.encode('utf-8'), server_address)
     if data:
-       sent = sock.sendto(data, address)
-   
-    for i in range (1, n + 1):
-        data1 = data + "(" + str(i) + ")"
-        print ("data sent: " + data)
-#       s.sendall(data.encode('utf-8'))
-        s.sendto(data1.encode('utf-8'), server_address)
+       data, address = sock.recvfrom(port)
 s.shutdown(1)
